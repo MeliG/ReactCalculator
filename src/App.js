9f7b36c1cd707c5803acbec1431119918915e80e
Melissa goh <melissa.goh01@gmail.com>
2017-10-08 15:31:03 +0800

    update css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   state= {
     currentAnswer: 0,
     displayNumber: '',
-    currentOperator:'',
+    currentOperator:'+',
     isFirstNumber:true,//the first number of the display
     answerMode:false //display number or answer accordingly
   }
@@ -33,7 +33,7 @@ class App extends Component {
 
   subtract = () => {
     this.setState({
-      currentAnswer:this.state.currentAnswer + Number(this.state.displayNumber),
+      currentAnswer:this.state.currentAnswer - Number(this.state.displayNumber),
       currentOperator: '-',
       isFirstNumber:true,
       answerMode:true
@@ -62,22 +62,27 @@ class App extends Component {
     this.setState({
       currentNumber: 0,
       currentAnswer:0,
-      displayNumber: ''
+      displayNumber: '',
     })
   }
 
   equal = () => {
-    let calculation
-    switch(this.state.currentOperator) {
-        case '+': calculation = this.add; break;
-        case '-': calculation = this.subtract; break;
-        case 'x': calculation = this.multiply; break;
-        case '/': calculation = this.divide; break;
-        default: this.setState({
-          answerMode:true
-        }); return;
+    if(this.state.currentOperator==='='){
+      return;
+    } else {
+      let calculation
+      switch(this.state.currentOperator) {
+          case '+': calculation = this.add; break;
+          case '-': calculation = this.subtract; break;
+          case 'x': calculation = this.multiply; break;
+          case '/': calculation = this.divide; break;
+          default: this.setState({
+            answerMode:true
+          }); return;
+      }
+      calculation()
     }
-    calculation()
+    this.setState({currentOperator:'='})
   }
 
   render() {
@@ -86,35 +91,35 @@ class App extends Component {
     console.log( ' ')
     return (
       <div className="App">
+        <div className="display">
+          {this.state.answerMode? this.state.currentAnswer : this.state.displayNumber}
+        </div>
         <div className="Numpad">
-          <p>
+          <div>
           <button onClick={this.assignValue}>1</button>
           <button onClick={this.assignValue}>2</button>
           <button onClick={this.assignValue}>3</button>
           <button onClick={this.add}>+</button>
-          </p>
-          <p>
+          </div>
+          <div>
           <button onClick={this.assignValue}>4</button>
           <button onClick={this.assignValue}>5</button>
           <button onClick={this.assignValue}>6</button>
           <button onClick={this.subtract}>-</button>
-          </p>
-          <p>
+          </div>
+          <div>
           <button onClick={this.assignValue}>7</button>
           <button onClick={this.assignValue}>8</button>
           <button onClick={this.assignValue}>9</button>
           <button onClick={this.divide}>/</button>
-          </p>
-          <p>
+          </div>
+          <div>
           <button onClick={this.assignValue}>0</button>
-          <button>.</button>
+          <button onClick={this.assignValue}>.</button>
           <button onClick={this.equal}>=</button>
           <button onClick={this.multiply}>x</button>
           <button onClick={this.clear}>c</button>
-          </p>
-        </div>
-        <div className="display">
-        ultimate Answer: {this.state.answerMode? this.state.currentAnswer : this.state.displayNumber}
+          </div>
         </div>
       </div>
     );
